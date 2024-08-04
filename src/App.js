96155640f140bc496e4db5cc9203351c748e627d
ShiluveloMakhubel <117033636+ShiluveloMakhubel@users.noreply.github.com>
2024-08-04 21:33:49 +0200

    update on app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-// src/App.js
 import React from 'react';
-//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import About from './Components/About';
 import Projects from './Components/Projects';
 import Education from './Components/Education';
 import Contact from './Components/Contact';
-import Skills from './Components/Skills'
+import Skills from './Components/Skills';
 import Courses from './Components/Courses';
 import './App.css';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
@@ -17,11 +16,9 @@ const App = () => {
       <About />
       <Education />
       <Projects />
-      <Skills/>
-      <Courses/>
-      <Contact />
-
-      
+      <Skills />
+      <Courses />
+      <Footer />
     </div>
   );
 };
