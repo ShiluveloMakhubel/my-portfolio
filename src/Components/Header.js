9edc4511babbe1e469302ba953f301ac6e731f5e
ShiluveloMakhubel <117033636+ShiluveloMakhubel@users.noreply.github.com>
2024-11-29 19:50:40 +0200

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css';
 import logo from './images/logo.PNG';
-
+import { Link } from 'react-scroll'; // Import Link from react-scroll
 
 const Header = () => {
   return (
@@ -10,15 +10,48 @@ const Header = () => {
         <div className="container">
           <a href="#about" className="brand">
             <img src={logo} alt="Shiluvelo Makhubele Logo" className="logo" />
-            
           </a>
           <ul className="nav-links">
-            <li><a href="#about">About</a></li>
-            <li><a href="#education">Education</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href='#courses'>Courses</a></li>
-            <li><a href='#Research'>Research</a></li>
+            <li>
+              <Link
+                to="about"
+                smooth={true}
+                duration={500}
+                offset={-50} // Adjust offset for fixed headers
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="education" smooth={true} duration={500} offset={-50}>
+                Education
+              </Link>
+            </li>
+            <li>
+              <Link to="experience" smooth={true} duration={500} offset={-50}>
+                Experience
+              </Link>
+            </li>
+            <li>
+              <Link to="projects" smooth={true} duration={500} offset={-50}>
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link to="contact" smooth={true} duration={500} offset={-50}>
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to="courses" smooth={true} duration={500} offset={-50}>
+                Courses
+              </Link>
+            </li>
+            <li>
+              <Link to="research" smooth={true} duration={500} offset={-50}>
+                Research
+              </Link>
+            </li>
           </ul>
         </div>
       </nav>
